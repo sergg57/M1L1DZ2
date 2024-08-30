da8fd05cb6b0b5aa7c4d93bedc7eb8614e9f4e01
sergg57 <sergg57@list.ru>
2024-08-30 12:00:48 +0300

    Add files via upload

---
+++ DZ[-2.py
@@ -0,0 +1,6 @@
+number_dz = 12
+number_hours = 1.5
+name_course = 'Python'
+time_for_task = number_hours / number_dz
+print('Курс:', name_course, ',', 'всего задач:', number_dz, ', затрачено часов: ', number_hours,
+      ',', 'среднее время выполнения:', time_for_task, 'часа.')
